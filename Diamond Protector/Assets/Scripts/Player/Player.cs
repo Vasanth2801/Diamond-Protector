fcaecb7e35769c4a7ab0c5293dbc789c9d1f0ed6
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-03 09:46:51 +0530

    Mouse Aim for the Player

--- Diamond Protector/Assets/Scripts/Player/Player.cs
+++ Diamond Protector/Assets/Scripts/Player/Player.cs
@@ -7,24 +7,37 @@ public class Player : MonoBehaviour
 
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Camera cam;
 
     [Header("Input Settings")]
     [SerializeField] private float moveInputX;
     [SerializeField] private float moveInputY;
+    [SerializeField] private Vector2 mousePos;
 
     private void Update()
     {
         moveInputX = Input.GetAxisRaw("Horizontal");
         moveInputY = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
         Move();
+
+        MouseLook();
     }
 
     private void Move()
     {
         rb.linearVelocity = new Vector2(moveInputX * speed, moveInputY * speed);    
+    }
+
+    private void MouseLook()
+    {
+        mousePos = mousePos - rb.position;
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
