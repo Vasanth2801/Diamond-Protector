742bcf6547a0606fdf692c77684fef2af42be7f8
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-03 16:47:26 +0530

    Player and Enemy Health

--- Diamond Protector/Assets/Scripts/Bullet/Bullet.cs
+++ Diamond Protector/Assets/Scripts/Bullet/Bullet.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    WeaponData currentWeapon;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         gameObject.SetActive(false);
+
+        if(collision.gameObject.tag == "Player")
+        {
+            EnemyHealth eh = collision.gameObject.GetComponent<EnemyHealth>();
+            if(eh != null)
+            {
+                eh.TakeDamage(currentWeapon.damage);
+            }
+        }
     }
 }
---
+++ Diamond Protector/Assets/Scripts/Bullet/EnemyBullet.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            PlayerHealth ph = collision.gameObject.GetComponent<PlayerHealth>();
+            if(ph != null)
+            {
+                ph.TakeDamage(10);
+            }
+        }
+    }
+}
